8f703c610f9f9abda67704f85d758c2c0d992d0b
Jannik Winghart <jannik.winghart@daimler.com>
2022-05-02 13:49:51 +0200

    set core.hardlink_lock on heroku

--- starter/main.py
+++ starter/main.py
@@ -13,6 +13,7 @@ app = FastAPI()
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("rm -f .dvc/tmp/lock")
     os.system("dvc config core.no_scm true")
+    os.system("dvc config core.hardlink_lock true")
     dvc_output = subprocess.run(["dvc", "pull"], capture_output=True, text=True)
     print(dvc_output.stdout)
     print(dvc_output.stderr)
