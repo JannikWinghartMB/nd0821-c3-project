cef5afe8f1cb5f5d390c30087c7f6bca555da870
Jannik Winghart <jannik.winghart@daimler.com>
2022-05-02 14:22:00 +0200

    force delete .dvc

--- starter/main.py
+++ starter/main.py
@@ -13,13 +13,14 @@ app = FastAPI()
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("rm -f .dvc/tmp/lock")
     os.system("dvc config core.no_scm true")
-    os.system("dvc config core.hardlink_lock true")
     dvc_output = subprocess.run(["dvc", "pull"], capture_output=True, text=True)
     print(dvc_output.stdout)
     print(dvc_output.stderr)
     if dvc_output.returncode != 0:
         print("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+    else:
+        print("dvc pull successful")
+    os.system("rm -r .dvc .apt/usr/lib/dvc -f")
 
 
 class PersonData(BaseModel):
